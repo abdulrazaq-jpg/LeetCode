cc2491b19b7ab73fb2432aec9b3f5ba79fac6daf
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-25 13:47:46 +0530

    Time: 3 ms (50.70%) | Memory: 23.7 MB (81.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -4,27 +4,59 @@ public:
         
         int n = nums.size();
 
-        unordered_map<int,int> map;
+        int cand_1 = -1e9;
+        int cand_2 = -1e9;
+
+        int cnt_1 = 0;
+        int cnt_2 = 0;
 
         for(int i = 0; i < n; i++)
         {
-            map[nums[i]]++;
+            if(nums[i] == cand_1)
+            {
+                cnt_1++;
+            }
+            else if(nums[i] == cand_2)
+            {
+                cnt_2++;
+            }
+            else if(cnt_1 == 0)
+            {
+                cnt_1 = 1;
+                cand_1 = nums[i];
+            }
+            else if(cnt_2 == 0)
+            {
+                cnt_2 = 1;
+                cand_2 = nums[i];
+            }
+            else
+            {
+                cnt_1--;
+                cnt_2--;
+            }
+        }
+
+        cnt_1 = 0;
+        cnt_2 = 0;
+
+        for(int  i = 0 ; i < n ;i++)
+        {
+            if(nums[i] == cand_1)
+            {
+                cnt_1++;
+            }
+
+            else if(nums[i] == cand_2)
+            {
+                cnt_2++;
+            }
         }
 
         vector<int> ans;
 
-        for(auto x: map)
-        {
-            int k = n/3;
-
-            int i = x.first;
-            int j = x.second;
-
-            if(j > k)
-            {
-                ans.push_back(i);
-            }
-        }
+        if(cnt_1 > n/3) ans.push_back(cand_1);
+        if(cnt_2 > n/3) ans.push_back(cand_2);
 
         return ans;
     }

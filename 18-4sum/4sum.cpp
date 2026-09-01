52b5050b22aa1fe61837626cb8a14e3a4c846a69
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-09-01 14:00:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.50%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -14,10 +14,18 @@ public:
                 continue;
             }
 
+            if ((long long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+
+            if ((long long)nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
+
             for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
+
+                if ((long long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+
+                if ((long long)nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
 
                 int l = j + 1, r = n - 1;
 

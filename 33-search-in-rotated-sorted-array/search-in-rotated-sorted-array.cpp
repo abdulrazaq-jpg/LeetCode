9a41650cb3fa9d14cf97b3384be40f82a9c8e003
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-09-04 09:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,48 +1,31 @@
 class Solution {
-private:
-    int bs(vector<int>& nums, int target, int low, int high) {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
         while (low <= high) {
             int mid = (low + high) / 2;
 
             if (nums[mid] == target)
                 return mid;
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] > target) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && nums[high] >= target) {
+                    low = mid + 1;
+                } 
+                else {
+                    high = mid - 1;
+                }
             }
         }
-
         return -1;
-    }
-
-public:
-    int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1, min = nums[0], min_ind = 0;
-
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
-
-            min_ind = low;
-        }
-
-        if (nums[min_ind] == target)
-            return min_ind;
-
-        int ans1 = bs(nums, target, 0, min_ind - 1);
-        int ans2 = bs(nums, target, min_ind, nums.size() - 1);
-
-        if (ans1 != -1) {
-            return ans1;
-        } else if (ans2 != -1) {
-            return ans2;
-        } else {
-            return -1;
-        }
     }
 };
